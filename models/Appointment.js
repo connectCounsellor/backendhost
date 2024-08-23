0fd34a18e44540dd5569b87c8c6a72cdb5e0cb03
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-08-23 23:47:29 +0530

    changes in appointment model

--- models/Appointment.js
+++ models/Appointment.js
@@ -13,7 +13,7 @@ const appointmentSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["pending", "accepted", "rejected"],
+    enum: ["pending", "accepted", "rejected","completed"],
     default: "pending",
   },
   reason: { type: String, required: true },
