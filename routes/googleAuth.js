72de5180c16d7a28b5dfb0dbb5b9c82a480faecd
prathmesh_khandare <prathameshkhandare1551@gmail.com>
2024-11-20 15:24:31 +0530

    google auth for app

--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-
+const usermodel = require('../models/userModel');
 // Initiate Google OAuth
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
@@ -24,5 +24,42 @@ router.get(
     }
   }
 );
+
+router.post('/sign-in-google-app',async (req, res) => {
+  const {given_name,family_name,email_verified,email} =req.body;
+  try{
+    if(!email_verified) {
+      return res.status(400).json({ message: 'Email is not verified' });
+    }
+    const user =usermodel.findOne({ email: email});
+    if(user){
+      const token = jwt.sign({ id: user._id },process.env.JWT_SECRET, { expiresIn: '30d' } );
+    return  es.status(200).json({ message: "Login successful", token });
+    }
+  
+    const new_user = await usermodel.create({
+      email: email,
+      firstName: given_name,
+      lastName: family_name,
+  
+    })
+  
+    
+   const token = jwt.sign({ id: new_user._id },process.env.JWT_SECRET, { expiresIn: '30d' } );
+      res.status(200).json({ message: "Login successful", token });
+  
+  }
+  catch(err){
+    res.status(500).json({ message: err.message});
+
+  }
+
+  
+})
+
+
+
+
+
 
 module.exports = router;
